ae13cda4fd126750bfa2a12858cf6eaf80b9bc13
Madmat8 <Mateka6400@gmail.com>
2023-08-10 16:55:00 +0200

    add spring maven plugin

--- src/main/resources/archetype-resources/src/test/java/__applicationName__Tests.java
+++ src/main/resources/archetype-resources/src/test/java/__applicationName__Tests.java
@@ -23,7 +23,7 @@ package ${package};
  * #L%
  */
 
-import ${package}.${modelName.toLowerCase()}.sdk.${modelName.toLowerCase()}.${modelName.toLowerCase()}.Person;
+import ${package}.sdk.${modelName}.${groupId}.api.${modelName.toLowerCase()}.${modelName.toLowerCase()}.person.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
